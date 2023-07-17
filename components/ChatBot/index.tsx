d4ac37cc477e1d76cb9a4dba0e967d463e16c615
gabrielkimani <kimgraphix0@gmail.com>
2023-07-17 12:39:57 +0300

    initial

--- components/ChatBot/index.tsx
+++ components/ChatBot/index.tsx
@@ -5,13 +5,13 @@ const Chatbot = () => {
     // Chatbot code
     window.__be = window.__be || {};
     window.__be.id = "64b508e6ebb51c00076dbe42";
-    (function() {
+    (function () {
       var be = document.createElement('script');
       be.type = 'text/javascript';
       be.async = true;
       be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
       var s = document.getElementsByTagName('script')[0];
-      s.parentNode.insertBefore(be, s);
+      s.parentNode?.insertBefore(be, s);
     })();
   }, []);
 
